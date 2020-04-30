228a2c829ccbff4f9fe254df69acf85c5f26e37e
Joe Palala <jose.palala@live.com>
2020-04-30 12:32:41 +0800

    feature - weather header footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AboutModule } from './about/about.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+HeaderComponent,
+FooterComponent,
     AboutModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
