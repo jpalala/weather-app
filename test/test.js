a08b570f68b34e6a2976780883da5470cfc10f1d
Joe Palala <jose.palala@live.com>
2020-05-06 13:54:27 +0800

    feat: added tests to api
    tests only that we can run a json server with the proper format
    modified `db.json` for this to work
    added `raw.json` (demo json from openweather)

--- test/test.js
+++ test/test.js
@@ -1,22 +1,42 @@
-var assert = require('assert');
+const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const jsonServer = require('json-server');
+const app = jsonServer.create('./api/db.json');
+const router = jsonServer.router('./api/router.json');
+const middlewares = jsonServer.defaults();
+var expect = chai.expect;
+let server;
+
+chai.use(chaiHttp);
+
 
 describe('Run local api test', function() {
-	before(function() {
-const { exec } = require('child_process');
-exec('npm run api', (err, stdout, stderr) => {
-  if (err) {
-    //some err occurred
-    console.error(err)
-  } else {
-   // the *entire* stdout and stderr (buffered)
-   console.log(`stdout: ${stdout}`);
-   console.log(`stderr: ${stderr}`);
-  }
-});
-	});
-  describe('get an api on returning json', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+
+  describe('test that local server is running', function() {
+    before(function() {
+      // run a local json server
+      app.use(middlewares);
+      app.use(router);
+      server = app.listen(3000, () => {
+        console.log('JSON Server is running');
+      });
     });
+
+    it('should return some sort of json data', function(done) {
+      chai.request(server)
+      .get('/forecasts')
+      .end(function(err, res) {
+        expect(res).to.be.json;
+        done();
+      });
+    });
+
+    after(function(){
+      server.close(function () {
+        console.log('tests done, server closing');
+      });
+    })
   });
 });
